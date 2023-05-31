0bd01861926090bd96e185ee0ba5658161be20c8
qingyiyiyi <303279510@qq.com>
2023-05-31 19:10:57 +0800

    商家的修改 	modified:   "src/components/\345\225\206\345\256\266/\345\225\206\345\256\266\345\276\205\345\244\204\347\220\206\350\256\242\345\215\225\351\241\265\351\235\242.vue" 	modified:   "src/components/\345\225\206\345\256\266/\345\225\206\345\256\266\345\272\227\351\223\272\351\241\265\351\235\242.vue" 	modified:   "src/components/\345\225\206\345\256\266/\345\225\206\345\256\266\346\200\273\351\241\265\351\235\242.vue" 	modified:   "src/components/\345\225\206\345\256\266/\345\225\206\345\256\266\347\231\273\345\275\225\351\241\265\351\235\242.vue" 	modified:   src/router/routes.js

---
+++ src/components/商家/商家待处理订单页面.vue
@@ -0,0 +1,8 @@
+<template>
+   <div class="背景"> 
+    <div class="单个订单区域" v-for="(Order,index) in 订单列表">
+        <div class="划分区域_子类纵向排列">
+            <div>订单id:<br>{{ Order.order.id }}</div>
+            <div>下单时间:<br>{{ Order.order.time }}></div>
+            <div>配送地址:<br>{{  }}</div>
+</template>
