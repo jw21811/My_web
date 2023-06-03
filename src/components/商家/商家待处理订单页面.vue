612b3f8c50015fe145fa0e6c3c4e55e1d2129f78
LCH <jw21811@126.com>
2023-06-03 10:32:26 +0800

    基本完成 	modified:   "src/components/\345\225\206\345\256\266/\345\225\206\345\256\266\345\276\205\345\244\204\347\220\206\350\256\242\345\215\225\351\241\265\351\235\242.vue"

--- src/components/商家/商家待处理订单页面.vue
+++ src/components/商家/商家待处理订单页面.vue
@@ -2,24 +2,35 @@
     <div class="背景">
         <div class="单个订单区域" v-for="(Order, index) in 订单总表">
                 <div class="划分区域_子类纵向排列">
-                    <div>订单id:<br>{{ Order.detail.orderDetail.order_id }}</div>
+                    <div>订单id:<br>{{ Order.order.id }}</div>
                     <div>下单时间：<br>{{ Order.order.time }}</div>
                     <div>
-                        <el-button type="primary" @click="确认完成制作(Order.id)">完成制作</el-button>
+                        <el-button type="primary" @click="确认完成制作(Order.order.id)">完成制作</el-button>
                     </div>
                 </div>
                 <div class="划分区域_子类纵向排列">
                     <div>配送地址：<br>{{ Order.order.customer_address }}</div>
-                    <div>订单总价：<br>{{ Order }} 元</div>
+                    <div>订单总价：<br>{{ Order.order.total_price }} 元</div>
+                    <div>订单备注：<br>{{ Order.order.notes }} </div>
                 </div>
                 <div class="划分区域_子类纵向排列">
-                    <div>品名：<br>{{ Order.订单配送地址 }}</div>
+                    <div>品名:</div>
+                    <div v-for="(good,index) in Order.detail">
+                    <br>{{good.goodsDetail.name}}
+                    </div>
                     
-
                 </div>
                 <div class="划分区域_子类纵向排列">
-                    <div>规格<br></div>
-
+                    <div>规格:</div>
+                    <div v-for="(good,index) in Order.detail">
+                        <br>{{good.orderDetail.specification}}
+                    </div>
+                </div>
+                <div class="划分区域_子类纵向排列">
+                    <div>数量:</div>
+                    <div v-for="(good,index) in Order.detail">
+                        <br>{{good.orderDetail.num}}
+                    </div>
                 </div>
         </div>
     </div>
@@ -40,18 +51,21 @@ export default {
     },
     mounted() {
         this.merchant_id = this.$route.query.merchant_id
-
-        var address = '/merchant/getPendingOrders?merchant_id=' + this.merchant_id
-        console.log(address)
-        this.axios
-            .get(address)
-            .then((Return_info) => {
-                this.订单总表 = Return_info.data.detail
-            })
-        this.解析订单总表()
+        this.向后端获取待处理订单()
     },
-    methods:
+    methods:    
     {
+        向后端获取待处理订单()
+        {
+            var address = '/merchant/getPendingOrders?merchant_id=' + this.merchant_id
+            console.log(address)
+            this.axios
+                .get(address)
+                .then((Return_info) => {
+                    this.订单总表 = Return_info.data.detail
+                    console.log(this.订单总表)
+                })
+        },
         Alert_Error(msg) {
             /**弹窗警告
              * 类型：错误
@@ -79,7 +93,7 @@ export default {
             })
         },
         向后端修改订单状态(id) {
-            var address = '/merchant/setOrderStatus?merchant_id=' + this.merchant_id + '&order_id=' + id
+            var address = '/merchant/setOrderPreparing?merchant_id=' + this.merchant_id + '&order_id=' + id
             console.log(address)
             this.axios
                 .get(address)//向后端接口传输
@@ -87,6 +101,7 @@ export default {
                     if(Return_info.data.status_code == 666)
                     {
                         this.Alert_Success('设置订单状态成功!')
+                        this.向后端获取待处理订单()
                     }
                     else
                     {
